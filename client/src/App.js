c0ed6a6c86cd40dea0e89db18776ae6ad0512e4a
Tran Hoang Kim <101057915+HoangKim1504@users.noreply.github.com>
2024-06-08 10:03:44 +0700

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,18 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
-import Profile from "./pages/Profile";
+import Profile from "./pages/profile/Profile";
+import ProfileEdit from "./pages/profile/ProfileEdit";
 import PrivateRoute from "./routes/PrivateRoute";
 import PublicRoute from "./routes/PublicRoute";
 import ForgotPassword from "./pages/forgot-reset-password/ForgotPassword";
 import EmailSent from "./pages/forgot-reset-password/EmailSent";
 import ResetPassword from "./pages/forgot-reset-password/ResetPassword";
 import PasswordChanged from "./pages/forgot-reset-password/PasswordChanged";
+import CourseInfo from "./pages/course-detail/CourseInfo";
+import MemberList from "./pages/course-detail/MemberList";
+import Post from "./pages/course-detail/Post";
+import Classwork from "./pages/course-detail/Classwork";
 
 function App() {
   return (
@@ -91,12 +96,57 @@ function App() {
             </PrivateRoute>
           }
         />
+        {/* Course info page */}
+        <Route
+          path="/courses/:title/course-info"
+          element={
+            <PrivateRoute>
+              <CourseInfo />
+            </PrivateRoute>
+          }
+        />
+        {/* Post page */}
+        <Route
+          path="/courses/:title/post"
+          element={
+            <PrivateRoute>
+              <Post />
+            </PrivateRoute>
+          }
+        />
+        {/* Classwork page */}
+        <Route
+          path="/courses/:title/classwork"
+          element={
+            <PrivateRoute>
+              <Classwork />
+            </PrivateRoute>
+          }
+        />
+        {/* Member list page */}
+        <Route
+          path="/courses/:title/member-list"
+          element={
+            <PrivateRoute>
+              <MemberList />
+            </PrivateRoute>
+          }
+        />
         {/* Profile page */}
         <Route
           path="/profile"
           element={
             <PrivateRoute>
               <Profile />
+            </PrivateRoute>
+          }
+        />
+        {/* Profile edit page */}
+        <Route
+          path="/profile/edit"
+          element={
+            <PrivateRoute>
+              <ProfileEdit />
             </PrivateRoute>
           }
         />
