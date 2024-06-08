baedd5828ef3b65a21042ebbdabf57a00bbcdb0c
Tran Hoang Kim <101057915+HoangKim1504@users.noreply.github.com>
2024-06-08 09:53:06 +0700

    Update Home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,22 +3,39 @@ import axios from "axios";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Pagination from "react-bootstrap/Pagination";
-import CardComponent from "../components/CardComponent";
+import CardVerticalComponent from "../components/CardVerticalComponent";
 import NavbarComponent from "../components/NavbarComponent";
 import FooterComponent from "../components/FooterComponent";
 import "../App.css";
 
-function Home(props) {
+function Home({ token }) {
   const [courses, setCourses] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [totalPages, setTotalPages] = useState(1);
+  const [user, setUser] = useState([]);
+
+  // get user data
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/getUser", {
+        headers: { Authorization: `Bearer ${token}` },
+        withCredentials: true,
+      })
+      .then((response) => {
+        console.log("Token is valid:", response.data);
+        setUser(response.data);
+      })
+      .catch((error) => {
+        console.log("Error verifying token:", error);
+      });
+  }, [token]);
 
   // Fetch courses data from the server
   useEffect(() => {
     axios
       .get(
         "http://localhost:5000/courses",
-        { params: { page: currentPage } }, // Send the page number as a query parameter
+        { params: { page: currentPage }, user }, // Send the page number as a query parameter
         { withCredentials: true } // Include credentials in the request
       )
       .then((res) => {
@@ -26,13 +43,14 @@ function Home(props) {
         setTotalPages(res.data.totalPages);
       })
       .catch((err) => console.log(err));
-  }, [currentPage]);
+  }, [currentPage, user]);
 
   // Pagination
   let items = [];
   for (let number = 1; number <= totalPages; number++) {
     items.push(
       <Pagination.Item
+        className="pagination-item"
         key={number}
         active={number === currentPage}
         onClick={() => setCurrentPage(number)}
@@ -44,6 +62,7 @@ function Home(props) {
 
   return (
     <div>
+      {/* Navbar */}
       <NavbarComponent />
       {/* Title image */}
       <div className="p-3 text-center bg-image">
@@ -57,19 +76,22 @@ function Home(props) {
       </div>
       <br />
       {/* List of courses  */}
-      <Row xs={2} md={3} className="g-4">
-        {courses.map((course) => (
-          <Col key={course.id}>
-            <CardComponent
-              key={course.id}
-              image={course.image}
-              title={course.title}
-              description={course.description}
-            />
-          </Col>
-        ))}
-      </Row>
-      <br />
+      <div className="home-background">
+        <Row xs={2} md={3} className="g-4">
+          {courses &&
+            courses.map((course, index) => (
+              <Col key={index}>
+                <CardVerticalComponent
+                  image={course.image}
+                  title={course.title}
+                  semester={course.semester}
+                  teacher={course.teacher}
+                />
+              </Col>
+            ))}
+        </Row>
+      </div>
+
       {/* Pagination */}
       <Pagination className="pagination">{items}</Pagination>
       {/* Footer */}
